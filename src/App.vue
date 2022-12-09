57f7cb5a1c5ecab2eb26a527ba0bf56ea3c2ebf8
流小浪 <1161632110@qq.com>
2022-12-09 15:19:34 +0800

    项目初始化--导入vant组件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <!-- 路由出口 -->
     <router-view/>
     <h1>黑马头条</h1>
+    <div>
+    <i class="iconfont icon-dianzan" ></i>
+    </div>
+    <van-button type="info">信息按钮</van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 导入vant组件库及全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+// 注册组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
